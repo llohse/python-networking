6e1058d66084cf5d8b375a07e8d92cd2c62c051c
Leon Merten Lohse <leon@green-side.de>
2015-06-28 21:15:46 +0200

    lots of changes

---
+++ check_radius/check_radius_status.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import argparse
+import subprocess
+import sys
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-t', type=int, default=3)
+parser.add_argument('ip')
+parser.add_argument('port')
+parser.add_argument('secret')
+
+cmd='echo "Message-Authenticator = 0x00, Response-Packet-Type = Access-Accept" | radclient -c1 -r1 -q -Pudp -t {t} {ip}:{port} status {secret}'
+
+args = parser.parse_args()
+ret = subprocess.call(cmd.format(**vars(args)), shell=True)
+
+if ret == 0:
+  print('OK - Radius is running')
+  sys.exit(1)
+else:
+  print('FATAL - Radius unsresponsive')
+  sys.exit(2)
